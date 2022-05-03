fe0c0cb6192939da75e530af4d3f4fef122f7d7d
Anthony <aperryiii20@gmail.com>
2022-05-03 13:40:59 -0700

    pull

--- client/screens/Login.js
+++ client/screens/Login.js
@@ -6,13 +6,17 @@ const Login = () => {
   // create state for password
   return (
     <KeyboardAvoidingView style={styles.container}>
+      <Text style={styles.header}>Login</Text>
       <View style={styles.inputContainer}>
         <TextInput placeholder='Email' style={styles.input} />
         <TextInput placeholder='Password' style={styles.input} secureTextEntry />
       </View>
       <View style={styles.buttonContainer}>
         <TouchableOpacity onPress={()=>{}} style={styles.button}>
-          <Text style={styles.button}>Login</Text>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={()=>{}} style={styles.button}>
+          <Text style={styles.buttonText}>Sign Up</Text>
         </TouchableOpacity>
       </View>
     </KeyboardAvoidingView>
@@ -24,8 +28,42 @@ export default Login
 const styles = StyleSheet.create({
    container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header:{
+    fontSize: 35,
+    marginBottom: 15,
+    borderBottomColor: '#000',
+    borderBottomWidth: 5,
+  },
+  inputContainer: {
+    width: '80%',
+  },
+  input: {
+    backgroundColor: '#fff',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  buttonContainer: {
+    width: '60%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  button: {
+    backgroundColor: '#000',
+    width: '100%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: '700',
+    fontSize: 16,
   },
 });
