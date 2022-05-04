7cc773c0cfe6f52311196d7dc0a4631315e9ea58
Anthony <aperryiii20@gmail.com>
2022-05-04 11:36:02 -0700

    updated-login

--- client/screens/Login.js
+++ client/screens/Login.js
@@ -7,25 +7,56 @@ import {
   View,
   Image
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { DOMAIN_NAME } from "@env";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useUserData } from "../context/UserContext";
 
-const Login = () => {
+
+
+const Login = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { setUserData } = useUserData();
+
+const loginUser = () => {
+    return fetch(`http://${DOMAIN_NAME}:5050/user/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({  email, password }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setUserData({
+          id: data._id,
+          userName: data.userName,
+          email: data.email,
+        });
+        AsyncStorage.setItem("token", data.token);
+      })
+      .then(() => props.navigation.navigate("Home"))
+      .catch((err) => console.log(err));
+  };
+
   // create state for email
   // create state for password
   return (
     <KeyboardAvoidingView style={styles.container}>
       <Image style={styles.bitlogo} source={require("../assets/logo.png")} />
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Email" style={styles.input} />
-        <TextInput
+        <TextInput onChangeText={setEmail} placeholder="Email" style={styles.input} />
+        <TextInput onChangeText={setPassword}
           placeholder="Password"
           style={styles.input}
           secureTextEntry
         />
       </View>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={() => {}} style={styles.button}>
+        <TouchableOpacity onPress={loginUser} style={styles.button}>
           <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => props.navigation.navigate("Register")} style={styles.button}>
+          <Text style={styles.buttonText}>Sign Up</Text>
         </TouchableOpacity>
       </View>
     </KeyboardAvoidingView>
